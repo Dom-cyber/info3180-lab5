6f0c18b9cac57eb478f23496a9e4f15ebfc877a0
Dominique Outar <dominiqueout1234@gmail.com>
2020-05-22 22:56:46 -0500

    created my first Flask app

--- app/views.py
+++ app/views.py
@@ -31,11 +31,13 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        #if form.username.data:
             # Get the username and password values from the form.
+            username = form.username.data
+            password = form.password.data
 
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
@@ -43,11 +45,26 @@ def login():
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
 
+            user = UserProfile.query.filter_by(username=username).first()
+
+            if user is not None and check_password_hash(user.password, password):
+                remember_me = False
+
+                if 'remember_me' in request.form:
+                    remember_me = True
+
             # get user id, load into session
-            login_user(user)
+            login_user(user, remember=remember_me)
 
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            flash("Succesful login", "Succesful")
+
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for("secure"))  # they should be redirected to a secure-page route instead
+            # return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            else: 
+                
+            flash("Login Unsuccesful")
     return render_template("login.html", form=form)
 
 
