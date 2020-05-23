d6112666a2d91c7ad2e4553af156f41b84a6a9a8
Dominique Outar <dominiqueout1234@gmail.com>
2020-05-22 21:40:56 -0500

    finally works

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:alter database lab5 owner to lab5@localhost/lab5" #accidentally set the password to 'alter database lab5 owner to lab5'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
